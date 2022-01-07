f7cf2912b77687eccd8a86b3273020e48d614735
joann <joann.balraj@gmail.com>
2022-01-07 14:36:44 -0600

    updates

--- Individual Works/wrangle.py
+++ Individual Works/wrangle.py
@@ -184,45 +184,6 @@ def nulls_by_row(df):
 
 ########################################### Explore Zillow Dataframe ###########################################
 
-
-def summarize(df):
-    '''
-    summarize will take in a single argument (a pandas dataframe)
-    and output to console various statistics on said dataframe, including:
-    # .head()
-    # .info()
-    # .describe()
-    # value_counts()
-    # observation of nulls in the dataframe
-    '''
-    print('=====================================================\n\n')
-    print('Dataframe head: ')
-    print(df.head(3).to_markdown())
-    print('=====================================================\n\n')
-    print('Dataframe info: ')
-    print(df.info())
-    print('=====================================================\n\n')
-    print('Dataframe Description: ')
-    print(df.describe().to_markdown())
-    num_cols = [col for col in df.columns if df[col].dtype != 'O']
-    cat_cols = [col for col in df.columns if col not in num_cols]
-    print('=====================================================')
-    print('DataFrame value counts: ')
-    for col in df.columns:
-        if col in cat_cols:
-            print(df[col].value_counts())
-        else:
-            print(df[col].value_counts(bins=10, sort=False))
-    print('=====================================================')
-    print('nulls in dataframe by column: ')
-    print(nulls_by_col(df))
-    print('=====================================================')
-    print('nulls in dataframe by row: ')
-    print(nulls_by_row(df))
-    print('============================================')
-
-
-
 ### functions to create clusters and scatter-plot: ###
 
 def create_cluster(df, X, k):
